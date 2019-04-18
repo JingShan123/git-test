e1b16fe7c4c61b2d340cff4145d3adf23e7279e8
admin <admin@qq.com>
2019-04-18 19:50:59 +0800

    把本地工程添加到git

---
+++ git/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+        System.out.println("f");
+        System.out.println("g");
+    }
+}
